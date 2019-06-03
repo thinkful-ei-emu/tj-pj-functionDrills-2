efe459d834df9976fbd02bcbceb72defd7904b43
Tara Patel <t.jeanne.patel@gmail.com>
2019-06-03 16:04:45 -0500

    Added daysInMonth

--- script.js
+++ script.js
@@ -40,5 +40,52 @@ function decode(str) {
   }
 }
 
-console.log(decode('craft'));
+function daysInMonth(month,leapYear) {
+  let sentence;
+  switch(month) {
+    case "January":
+      sentence = "January has 31 days.";
+      break;
+    case "February":
+      if (leapYear) {
+        sentence = "February has 29 days.";
+      }
+      else {
+        sentence = "February has 28 days.";
+      }
+      break;
+    case "March":
+      sentence = "March has 31 days.";
+      break;
+    case "April":
+      sentence = "April has 30 days.";
+      break;
+    case "May":
+      sentence = "May has 31 days.";
+      break;
+    case "June":
+      sentence = "June has 30 days.";
+      break;
+    case "July":
+      sentence = "July has 31 days.";
+      break;
+    case "August":
+      sentence = "August has 31 days.";
+      break;
+    case "September":
+      sentence = "September has 30 days.";
+      break;
+    case "October":
+      sentence = "October has 31 days.";
+      break;
+    case "November":
+      sentence = "November has 30 days.";
+      break;
+    case "December":
+      sentence = "December has 31 days.";
+      break;    
+  }
+  return sentence;
+}
 
+daysInMonth("October");
