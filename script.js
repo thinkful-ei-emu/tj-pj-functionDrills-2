dd7cf1524fc6b792fd5b56b6f3174727c684c939
Prabhjot Singh <pjsingh1359@outlook.com>
2019-06-03 13:52:23 -0700

    added hidden message decoder

--- script.js
+++ script.js
@@ -25,3 +25,20 @@ function beyond(num) {
 }
 
 beyond(10);
+
+function decode(str) {
+  if(str.charAt(0) === 'a') {
+    return str.charAt(1);
+  } else if(str.charAt(0) === 'b') {
+    return str.charAt(2);
+  } else if(str.charAt(0) === 'c') {
+    return str.charAt(3);
+  } else if(str.charAt(0) === 'd') {
+    return str.charAt(4);
+  } else {
+    return ' ';
+  }
+}
+
+console.log(decode('craft'));
+
